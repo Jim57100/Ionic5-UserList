482d32b4cafe8428f2d8bb673e3bf1e62d26c1d1
Jean-Michael SALICE <jmsalice@gmail.com>
2022-01-17 13:35:10 +0100

    "add geoloc part2"

--- src/app/geolocation/geolocation.page.ts
+++ src/app/geolocation/geolocation.page.ts
@@ -1,10 +1,9 @@
-import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { Plugins } from '@capacitor/core';
 import { Geolocation } from '@capacitor/geolocation';
 import { Observable } from 'rxjs';
-import { ThrowStmt } from '@angular/compiler';
+import { map } from 'rxjs/operators';
 
 declare var google;
 
@@ -14,21 +13,53 @@ declare var google;
   styleUrls: ['./geolocation.page.scss'],
 })
 
-export class GeolocationPage implements OnInit {
+export class GeolocationPage {
 
   locations: Observable<any>;
   locationsCollection: AngularFirestoreCollection<any>;
+  
   user = null;
+  isTracking = false; 
+  watch: any;
+ 
 
-  @ViewChild('map') mapElement: ElementRef;
+  @ViewChild('map', { static: false }) mapElement: ElementRef;
   map:any;
   markers = [];
+
   constructor(private afAuth: AngularFireAuth, private locateCollection :AngularFirestore) { 
     this.anonLogin();
   }
 
-  ngOnInit() {
+  // ngOnInit() {
 
+  // }
+
+  anonLogin() {
+    this.afAuth.signInAnonymously().then(res => {
+      console.log(res.user.uid);
+      this.user = res.user;
+      this.locationsCollection = this.locateCollection.collection(
+        `locations/${this.user.uid}/track`,
+        ref => ref.orderBy('timestamp')
+      );
+
+      //load firebase data
+      this.locations = this.locationsCollection.snapshotChanges().pipe(
+        map(actions =>
+          actions.map(a => {
+            const data = a.payload.doc.data();
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })
+        )
+      );
+
+      //update map
+      this.locations.subscribe(locations => {
+        this.updateMap(locations);
+      });
+    })
   }
 
   ionViewWillEnter() {
@@ -48,19 +79,61 @@ export class GeolocationPage implements OnInit {
     this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
   }
 
-  anonLogin() {
-    this.afAuth.signInAnonymously().then(user => {
-      console.log(user);
-      this.user = user;
-      this.locationsCollection = this.locateCollection.collection(
-        `locations/${this.user.uid}/track`,
-        // ref = ref.orderBy('timestamp')
-      );
-      //load firebase data
-
-      //update map
-    })
+  // Use Capacitor to track our geolocation
+  startTracking() {
+    this.isTracking = true;
+    this.watch = Geolocation.watchPosition({}, (position, err) => {
+      if (position) {
+        this.addNewLocation(
+          position.coords.latitude,
+          position.coords.longitude,
+          position.timestamp
+        );
+      }
+    });
   }
 
+  // Unsubscribe from the geolocation watch using the initial ID
+  stopTracking() {
+    Geolocation.clearWatch({ id: this.watch }).then(() => {
+      this.isTracking = false;
+    });
+  } 
 
+  // Save a new location to Firebase and center the map
+  addNewLocation(lat, lng, timestamp) {
+    this.locationsCollection.add({
+      lat,
+      lng,
+      timestamp
+    });
+  
+    //center the map
+    let position = new google.maps.LatLng(lat, lng);
+    this.map.setCenter(position);
+    this.map.setZoom(5);
+  }
+  // Delete a location from Firebase
+  deleteLocation(pos) {
+    console.log('delete: ', pos);
+    this.locationsCollection.doc(pos.id).delete();
+  }
+  
+  // Redraw all markers on the map
+  updateMap(locations) {
+    // Remove all current marker
+    this.markers.map(marker => marker.setMap(null));
+    this.markers = [];
+  
+    for (let loc of locations) {
+      let latLng = new google.maps.LatLng(loc.lat, loc.lng);
+  
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });
+      this.markers.push(marker);
+    }
+  }
 }
